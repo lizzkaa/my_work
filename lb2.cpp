57df60bd2a8beb6665b181c8f6b46b6d93e73b5b
lizzkaa <lizaisaeva09122004@gmail.com>
2024-02-16 04:50:55 +0300

    Create lb2.cpp
    вторая лаба

---
+++ lb2.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+class tetrahedron {
+    int a;
+    float s;
+    float v;
+
+public:
+    tetrahedron(int valueA)
+    {
+        a = valueA;
+        s = sqrt(3) * pow(a, 2);
+        v = (pow(a, 3) * sqrt(2)) / 12;
+    }
+    void print() {
+        std::cout << "сторона:" << a << ", Площадь поверхности:" << s << ", Объем" << v;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    int x;
+    std::cout << "Введите сторону тетраэдра:";
+    std::cin >> x;
+    tetrahedron a(x);
+    a.print();
+
+}
